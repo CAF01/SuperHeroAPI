91d9396f5f43d5a758cbe920a36c1a3956787e9c
Chris Alcantara <christian_pria@hotmail.com>
2023-04-01 14:13:53 -0600

    first commit

---
+++ SuperHeroAPI/Controllers/SuperHeroController.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SuperHeroAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SuperHeroController : ControllerBase
+    {
+        private readonly DataContext context;
+
+        public SuperHeroController(DataContext context)
+        {
+            this.context = context;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            return Ok(await context.SuperHeroes.ToListAsync());
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddHero(SuperHero superHero)
+        {
+            await context.SuperHeroes.AddAsync(superHero);
+            context.SaveChanges();
+            return Ok(await context.SuperHeroes.ToListAsync());
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById(int id)
+        {
+            var hero = await context.SuperHeroes.FindAsync(id);
+            if (hero == null)
+                return BadRequest("Hero not found");
+            else
+                return Ok(hero);
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> UpdateHero(SuperHero superhero)
+        {
+            var hero = await context.SuperHeroes.FindAsync(superhero.Id);
+            if (hero == null)
+                return BadRequest("Hero not found");
+            
+            hero.Name = superhero.Name;
+            hero.FirstName = superhero.FirstName;
+            hero.LastName = superhero.LastName;
+            hero.Place = superhero.Place;
+            await context.SaveChangesAsync();
+            return Ok(await context.SuperHeroes.ToListAsync());
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteHero(int id)
+        {
+            var hero = await context.SuperHeroes.FindAsync(id);
+            if (hero == null)
+                return BadRequest("Hero not found");
+
+            context.SuperHeroes.Remove(hero);
+            await context.SaveChangesAsync();
+            return Ok($"{hero.Name} deleted");
+        }
+    }
+}
---
+++ SuperHeroAPI/Data/DataContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace SuperHeroAPI.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> dbContextOptions) : base(dbContextOptions)
+        {}
+
+        public DbSet<SuperHero> SuperHeroes { get; set; }
+    }
+}
